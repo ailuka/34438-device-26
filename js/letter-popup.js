9c170ad25b625b27e05886d2d9552d2ac09288d0
Yulia Karpova <karpova.jul@gmail.com>
2019-10-16 23:54:47 +1100

    Подключила скрипт для оживления модального окна с формой отправки письма

---
+++ js/letter-popup.js
@@ -0,0 +1,66 @@
+var letterLink = document.querySelector(".letter-link");
+
+var letterPopup = document.querySelector(".modal-letter");
+var letterClose = letterPopup.querySelector(".modal-close");
+
+var letterForm = letterPopup.querySelector(".letter-form");
+var letterSenderName = letterPopup.querySelector("[name=name]");
+var letterSenderEmail = letterPopup.querySelector("[name=email]");
+var letterSenderMessage = letterPopup.querySelector("[name=message]");
+
+var isStorageSupport = true;
+var storageName = "";
+var storageEmail = "";
+
+try {
+  storageName = localStorage.getItem("name");
+  storageEmail = localStorage.getItem("email");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+letterLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  letterPopup.classList.add("modal-show");
+
+  letterSenderName.value = storageName;
+  letterSenderEmail.value = storageEmail;
+
+  if (storageName && storageEmail) {
+    letterSenderMessage.focus();
+  } else if (storageName) {
+    letterSenderEmail.focus();
+  } else {
+    letterSenderName.focus()
+  }
+});
+
+letterClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  letterPopup.classList.remove("modal-show");
+  letterPopup.classList.remove("modal-error");
+});
+
+letterForm.addEventListener("submit", function (evt) {
+  if (!letterSenderName.value || !letterSenderEmail.value || !letterSenderMessage.value) {
+    evt.preventDefault();
+    letterPopup.classList.remove("modal-error");
+    letterPopup.offsetWidth = letterPopup.offsetWidth;
+    letterPopup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("name", letterSenderName.value );
+      localStorage.setItem("email", letterSenderEmail.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (letterPopup.classList.contains("modal-show")) {
+      letterPopup.classList.remove("modal-show");
+      letterPopup.classList.remove("modal-error");
+    }
+  }
+});
